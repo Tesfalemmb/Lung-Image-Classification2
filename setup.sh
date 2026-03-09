ee2878f69e408bda9f897f6be362fb67e33dee2c
Tesfalemmb <74550157+Tesfalemmb@users.noreply.github.com>
2026-03-09 17:39:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install --upgrade pip
-pip install tensorflow==2.15.0 matplotlib==3.7.5 opencv-python-headless==4.8.1.78 protobuf==3.20.3 Pillow==10.1.0 numpy==1.24.3 h5py==3.9.0
+pip install tensorflow==2.17.0 matplotlib==3.9.2 opencv-python-headless==4.10.0.84 protobuf==3.20.3 Pillow==10.4.0 numpy==1.26.4 h5py==3.11.0
